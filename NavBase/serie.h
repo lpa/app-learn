6b53d6cc2a1a34d67c0e010be8c0a9f3b087e80d
lpa <goj73@hotmail.com>
2010-11-27 20:42:15 +0100

    added files

---
+++ NavBase/serie.h
@@ -0,0 +1,16 @@
+//
+//  serie.h
+//  NavBase
+//
+//  Created by Laurent on 27/11/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface serie : NSObject {
+
+}
+
+@end
---
+++ NavBase/serie.m
@@ -0,0 +1,14 @@
+//
+//  serie.m
+//  NavBase
+//
+//  Created by Laurent on 27/11/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "serie.h"
+
+
+@implementation serie
+
+@end
